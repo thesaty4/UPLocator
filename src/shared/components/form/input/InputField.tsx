cefc0908bf3ac4a70bb9a6f9fb98ca7868563352
Satya Mishra <satya.mishra@bigohtech.com>
2024-03-10 13:36:19 +0530

    input and app

---
+++ src/shared/components/form/input/InputField.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const InputField = () => {
+  return (
+    <View>
+      <Text>InputField</Text>
+    </View>
+  );
+};
+
+export default InputField;
