6f6714ab91b73498038f90a92dc035998f3bffb2
Satya Mishra <satya.mishra@frankieone.com>
2024-03-17 17:48:00 +0530

    routing change

---
+++ src/pages/features/PoleList/PoleListMain.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PoleList from './PoleList';
+
+const PoleListMain = () => {
+  return <PoleList />;
+};
+
+export default PoleListMain;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Icons} from '../../assets/icons/all-icons';
 import CapturePole from '../features/CapturePole/CapturePole';
-import PoleList from '../features/PoleList/PoleList';
 import TabView, {TabViewProps} from '../../shared/view/TabView/TabView';
 import {Routes, router} from '../../shared/routes/router';
 import Zonal from '../features/Zonal/Zonal';
+import PoleListMain from '../features/PoleList/PoleListMain';
 
 const Home = () => {
   return <TabView {...tabViewInfo} />;
@@ -28,7 +28,7 @@ const tabViewInfo: TabViewProps<Routes> = {
     {
       icon: Icons.menuList,
       route: router['poleList'],
-      element: PoleList,
+      element: PoleListMain,
     },
   ],
 };
