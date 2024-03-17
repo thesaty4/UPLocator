efe353a39b8752e6eea4716ce6f5f5876d2518a8
Satya Mishra <satya.mishra@frankieone.com>
2024-03-17 21:23:15 +0530

    improve camera

--- src/components/CameraView/CameraView.tsx
+++ src/components/CameraView/CameraView.tsx
@@ -19,16 +19,42 @@ interface CameraViewProps {
   onCapture?: (image: TakePictureResponse) => void;
 }
 
+type ZoomInfo = {
+  label: string;
+  value: number;
+  isActive?: boolean;
+};
+
 const cameraConfig = {
   visible: true,
   hasPermission: false,
 };
+
+const zoomRatio: ZoomInfo[] = [
+  {
+    label: 'Normal',
+    value: 0,
+  },
+  {
+    label: '1X',
+    value: 10,
+  },
+  {
+    label: '2X',
+    value: 50,
+  },
+  {
+    label: '3X',
+    value: 100,
+  },
+];
 
 const CameraView: React.FC<CameraViewProps> = ({onCapture}) => {
   const [camera, setPermission] = useState(cameraConfig);
   const [flash, setFlash] = useState(false);
   const [isBackCam, setBackCam] = useState(true);
   const [isFullView, setFullView] = useState(false);
+  const [zoom, setZoom] = useState<ZoomInfo>(zoomRatio[0]);
 
   const {width, height} = Dimensions.get('window'); // Get screen dimensions
 
@@ -61,6 +87,82 @@ const CameraView: React.FC<CameraViewProps> = ({onCapture}) => {
     }
   };
 
+  const TopView = () =>
+    useMemo(
+      () => (
+        <View {...getStyles(['m2', 'p2', 'flex', 'flexRow', 'justifyBetween'])}>
+          <View {...getStyles(['flex', 'flexRow'], 'default', {gap: 20})}>
+            <TouchableOpacity
+              onPress={() => {
+                setFlash(prev => !prev);
+              }}>
+              <Image
+                source={flash ? Icons.flashOn : Icons.flashOff}
+                tintColor={appColors.white}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => {
+                setBackCam(prev => !prev);
+              }}>
+              <Image source={Icons.rotateCamera} tintColor={appColors.white} />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            onPress={() => {
+              setFullView(prev => !prev);
+            }}>
+            <Image source={Icons.expand} tintColor={appColors.white} />
+          </TouchableOpacity>
+        </View>
+      ),
+      [],
+    );
+
+  const BottomView = () =>
+    useMemo(
+      () => (
+        <View {...getStyles(['absolute', 'bottom0', 'wFull', 'itemsCenter'])}>
+          <View {...getStyles(['itemsCenter'])}>
+            <View {...getStyles(['flexRow', 'p5'], 'default', {gap: 20})}>
+              {zoomRatio.map(item => (
+                <TouchableOpacity
+                  onPress={() => {
+                    setZoom(item);
+                  }}>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: '900',
+                      color: 'white',
+                      opacity: zoom.label == item.label ? 1 : 0.5,
+                    }}>
+                    {item.label}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+            <TouchableOpacity
+              {...getStyles(
+                ['h20', 'w20', 'itemsCenter', 'justifyCenter', 'mB10'],
+                'default',
+                {
+                  borderRadius: 50,
+                  backgroundColor: appColors.primary,
+                },
+              )}
+              onPress={handleCapture}>
+              <Image
+                source={Icons.camera}
+                style={{tintColor: appColors.white, height: 40, width: 40}}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      ),
+      [],
+    );
+
   const Camera = () =>
     useMemo(() => {
       return (
@@ -74,61 +176,11 @@ const CameraView: React.FC<CameraViewProps> = ({onCapture}) => {
             })}
             type={isBackCam ? 'back' : 'front'}
             flashMode={flash ? 'on' : 'off'}
-            captureAudio={false}>
+            captureAudio={false}
+            zoom={zoom.value}>
             <View {...getStyles(['flex1', 'wFull'], 'default')}>
-              <View
-                {...getStyles([
-                  'm2',
-                  'p2',
-                  'flex',
-                  'flexRow',
-                  'justifyBetween',
-                ])}>
-                <View {...getStyles(['flex', 'flexRow'], 'default', {gap: 20})}>
-                  <TouchableOpacity
-                    onPress={() => {
-                      setFlash(prev => !prev);
-                    }}>
-                    <Image
-                      source={flash ? Icons.flashOn : Icons.flashOff}
-                      tintColor={appColors.primary}
-                    />
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    onPress={() => {
-                      setBackCam(prev => !prev);
-                    }}>
-                    <Image
-                      source={Icons.rotateCamera}
-                      tintColor={appColors.primary}
-                    />
-                  </TouchableOpacity>
-                </View>
-                <TouchableOpacity
-                  onPress={() => {
-                    setFullView(prev => !prev);
-                  }}>
-                  <Image source={Icons.expand} tintColor={appColors.primary} />
-                </TouchableOpacity>
-              </View>
-              <View
-                {...getStyles(['absolute', 'bottom0', 'wFull', 'itemsCenter'])}>
-                <TouchableOpacity
-                  {...getStyles(
-                    ['h20', 'w20', 'itemsCenter', 'justifyCenter', 'mB10'],
-                    'default',
-                    {
-                      borderRadius: 50,
-                      backgroundColor: appColors.primary,
-                    },
-                  )}
-                  onPress={handleCapture}>
-                  <Image
-                    source={Icons.camera}
-                    style={{tintColor: appColors.white, height: 40, width: 40}}
-                  />
-                </TouchableOpacity>
-              </View>
+              <TopView />
+              <BottomView />
             </View>
           </RNCamera>
         </View>
