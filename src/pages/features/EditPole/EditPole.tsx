60536b2a9a3abcbbf24b26d75a19576f6962b42e
Satya Mishra <satya.mishra@frankieone.com>
2024-03-17 18:47:53 +0530

    edit pole refactor

--- src/pages/features/EditPole/EditPole.tsx
+++ src/pages/features/EditPole/EditPole.tsx
@@ -1,17 +1,146 @@
 import React from 'react';
-import {View} from 'react-native';
+import {ScrollView, View} from 'react-native';
 import Header from '../../../shared/view/header/Header';
 import {Icons} from '../../../assets/icons/all-icons';
+import {getStyles} from '../../../shared/utils/modifiers';
+import {useForm} from 'react-hook-form';
+import InputField from '../../../shared/components/form/input/InputField';
+import Button from '../../../shared/components/form/button/Button';
+
+type EditPoleForm = {
+  zonal: string;
+  routeLine: string;
+  line: string;
+  poleNumber: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+  description: string;
+  status: boolean;
+};
 
 const EditPole = () => {
+  const {
+    control,
+    formState: {errors},
+    handleSubmit,
+  } = useForm<EditPoleForm>();
+
+  const onUpdate = (data: EditPoleForm) => {
+    console.log('data goes here...', data);
+  };
+
   return (
-    <View>
+    <View {...getStyles(['mB10'])}>
       <Header
         heading="Edit Pole Information"
         icon={Icons.history}
         isBack
         isGradient
       />
+      <ScrollView {...getStyles(['mY2'])}>
+        <InputField
+          name="zonal"
+          placeholder="Select Zonal"
+          rule={{
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+          }}
+          control={control}
+          errors={errors}
+          type="text"
+          variant="normal"
+        />
+        <InputField
+          name="routeLine"
+          placeholder="Route Line"
+          rule={{
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+          }}
+          control={control}
+          errors={errors}
+          type="text"
+          variant="normal"
+        />
+        <InputField
+          name="line"
+          placeholder="Enter Line"
+          rule={{
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+          }}
+          control={control}
+          errors={errors}
+          type="text"
+          variant="normal"
+        />
+        <InputField
+          name="poleNumber"
+          placeholder="Enter Pole No."
+          rule={{
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+          }}
+          control={control}
+          errors={errors}
+          type="text"
+          variant="normal"
+        />
+        <InputField
+          name="location.latitude"
+          placeholder="Latitude"
+          rule={{
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+          }}
+          control={control}
+          errors={errors}
+          type="text"
+          variant="normal"
+        />
+        <InputField
+          name="location.longitude"
+          placeholder="Longitude"
+          rule={{
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+          }}
+          control={control}
+          errors={errors}
+          type="text"
+          variant="normal"
+        />
+        <InputField
+          name="description"
+          placeholder="Write description..."
+          rule={{
+            required: {
+              value: true,
+              message: 'This field is required',
+            },
+          }}
+          control={control}
+          errors={errors}
+          type="text"
+          variant="normal"
+        />
+
+        <Button label="Update" onPress={handleSubmit(onUpdate)} />
+      </ScrollView>
     </View>
   );
 };
